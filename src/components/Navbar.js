667ac8f5ea0204b9d274a49e172b8d31847195b1
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-18 16:26:15 -0800

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+import { BrowserRouter, Route } from "react-router-dom"; 
+
+
+export default class Navbar extends Component {
+        render(){
+            return(
+                <nav>
+
+
+                </nav>
+
+            )
+
+
+        }
+
+
+
+}
