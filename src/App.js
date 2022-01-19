4dc0b9c0ed93254c7705fd3317e56e6110495665
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-18 16:26:20 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react'; 
-import Pokecard from'./Pokecard'; 
+import Pokecard from './components/Pokecard'
 
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Pokecard 
+      {/* <Pokecard 
       id={4}
       name="Charmander"
       type="fire"
       exp={62}
     
-      />
+      /> */}
     </div>
   );
 }
