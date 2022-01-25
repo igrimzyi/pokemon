d0942c476bcfccd62b2e350a35aa83ade26b84c7
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-25 15:38:29 -0800

    Update Pokedex.js

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,1 +1,14 @@
-import React from "react";
+import React, {Component} from "react";
+
+export default class Pokedex extends Component{
+    render(){
+            return(
+                    <div>
+                        <h1>This is a pokedex</h1>
+                    </div>
+                )
+
+        }
+
+
+}
