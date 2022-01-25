b8e798dc048b36914794a1df0288fef7e921cc21
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-25 15:26:01 -0800

    Edited Navbar// temporary edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
-import { BrowserRouter, NavLink, Route } from "react-router-dom"; 
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"; 
+import Pokecard from "./Pokecard";
 
 
 export default class Navbar extends Component {
@@ -12,11 +13,13 @@ export default class Navbar extends Component {
                         <NavLink to="/pokedex">Pokedex</NavLink>
 
                     </nav>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/pokedex" exact component={Pokedex}/>
+                    <Routes>
+                    <Route path="/" exact component={Pokecard}/>
+                    {/* <Route path="/pokedex" exact component={Pokedex}/> */}
                     {/* <Route path="/game" exact component={Game}/>
                     <Route path="/creator" exact component={Creator}/>
                     <Route path="/pokedex" exact component={About}/> */}
+                    </Routes>
 
                 </div>  
 
