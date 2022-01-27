a8a68c8388da0578dc8d406ea36c44d7ae33eeb7
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-27 11:42:31 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,18 @@ import Pokedex from "./Pokedex";
 export default class Navbar extends Component {
         render(){
             return(
-            <BrowserRouter>
+
                 <div>
                     <nav>
                         <NavLink to="/">Home</NavLink>
                         <NavLink to="/pokedex">Pokedex</NavLink>
 
                     </nav>
-                    <Routes>
-                    <Route path="/" exact element={Pokecard}/>
-                    <Route path="/pokedex" exact element={Pokedex}/>
-                    {/* <Route path="/game" exact component={Game}/>
-                    <Route path="/creator" exact component={Creator}/>
-                    <Route path="/pokedex" exact component={About}/> */}
-                    </Routes>
+                    
 
                 </div>  
 
-            </BrowserRouter>
+            
 
             )
 
