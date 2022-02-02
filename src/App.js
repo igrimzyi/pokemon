545faf8c390e1579011b63b1fe62af284fb19a2c
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-01 16:29:46 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div>
           <Routes>
             <Route path='/' element={Home}/>
-            <Route path='/pokedex' Component={Pokedex}/>
+            <Route path='/pokedex' element={Pokedex}/>
             
 
 
