af5ce6bed3c40bbbc4612e6595807e240063e31d
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-02 21:58:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'; 
+import React from 'react'; 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Pokedex from './components/Pokedex';
