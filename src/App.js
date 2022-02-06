d869a0232784a824f08ccc6dd04ead1d61edb509
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-05 20:45:10 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
   render(){
     return (
      <div className="App">
+      {/* Always wrap routes in router component */}
         <Router>
         <div>
            <Navbar/>
