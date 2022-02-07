450e8e05295044ac5a0aecfc2f5939384b203c38
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-07 12:12:51 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+//here is where i would import any global dependencies 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"; 
 
 
 ReactDOM.render(
