50fd6219af85c361c2448937a3b8c75bfb9b099b
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-13 13:54:41 -0800

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,12 @@ import React,{ Component } from "react";
 export default class Game extends Component {
         render(){
             return(
-                    <div>This is the game portion</div>
+                    <div>
+                This is the game portion
+                <span>this will be including the the generated game</span>
+
+                </div>
+                        
                 )
         }
 
