ae6f79c430a9eba6307505a5beebe1fdd767f3cc
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-15 10:48:15 -0800

    Create Home.js

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+ class Home extends Component{
+        render(){
+            return(
+                <div>
+                    <span className="mb-auto">Welcome to Poke Complex</span>
+                    <h3>Check out the pokedex and the game that comes with!</h3>
+
+                </div>
+                )
+      }
+
+    }
+
+export default Home;
+
