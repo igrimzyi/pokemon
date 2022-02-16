29e88cb8b36a67503afa641ab19f943a93b39075
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-16 11:51:51 -0800

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,29 @@
 import React, {Component} from "react";
 import './home.css'
+import {Button} from 'reactstrap'
 
  class Home extends Component{
         render(){
             return(
-                <div>
+                <section className="main">
+                <div className="main">
+                    
                     <div className="title">
                         <h3 className="mb-auto">Welcome to Poke Complex</h3>
                         <span>Check out the pokedex and the game that comes with!</span>
-
+                        <div>
+                    <Button
+                    color="dark"
+                    size=""
+                    >
+                    View Pokedex
+                    </Button>
                     </div>
+                    </div>
+                    
+                    
                 </div>
+                </section>
                 )
       }
 
