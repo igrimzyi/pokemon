b7d32d43cc080932617dbaeca20297549371f8b3
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-17 23:14:56 -0800

    unused code

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"; 
+import {NavLink  } from "react-router-dom"; 
 import {  Navbar, 
 NavbarBrand, 
 NavbarToggler, 
