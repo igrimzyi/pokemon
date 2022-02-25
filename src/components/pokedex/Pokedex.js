41dfaa5a40b7d24f6ec1665d96619b930759bb2a
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-25 15:34:08 -0800

    Adding Cards for Pokedex!
    Adding cards for poke "sprites"  that will allow the user to like a pokemon and see stats

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,11 +1,41 @@
 import React, {Component} from "react";
+import { CardGroup, 
+Card, 
+CardImg, 
+CardSubtitle,
+CardText} from "reactstrap";
+import './pokedex.css'
 
 class Pokedex extends Component{
     render(){
             return(
-                    <div className="title">
-                        <h1>This is the pokedex</h1>
-                    </div>
+                    <CardGroup>
+                     <Card>
+    <CardImg
+      alt="Card image cap"
+      src="https://picsum.photos/318/180"
+      top
+      width="100%"
+    />
+    <CardBody>
+      <CardTitle tag="h5">
+        Card title
+      </CardTitle>
+      <CardSubtitle
+        className="mb-2 text-muted"
+        tag="h6"
+      >
+        Card subtitle
+      </CardSubtitle>
+      <CardText>
+        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+      </CardText>
+      <Button>
+        Button
+      </Button>
+    </CardBody>
+  </Card>
+                    </CardGroup>
                 )
 
         }
