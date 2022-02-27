b6afd7203bee7c0abdecb9798dba32bd1bfb3c5d
igrimzyi <1isaiahofficial1@gmail.com>
2022-02-26 21:51:45 -0800

    Update Pokedex.js

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -9,33 +9,36 @@ import './pokedex.css'
 class Pokedex extends Component{
     render(){
             return(
-                    <CardGroup>
-                     <Card>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Pokemon
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Level
-      </CardSubtitle>
-      <CardText>
-        This is an overview of the Pokemon
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-                    </CardGroup>
+
+    <h1 className="style">helllo</h1>
+
+//                     <CardGroup>
+//                      <Card>
+//     <CardImg
+//       alt="Card image cap"
+//       src="https://picsum.photos/318/180"
+//       top
+//       width="100%"
+//     />
+//     <CardBody>
+//       <CardTitle tag="h5">
+//         Pokemon
+//       </CardTitle>
+//       <CardSubtitle
+//         className="mb-2 text-muted"
+//         tag="h6"
+//       >
+//         Level
+//       </CardSubtitle>
+//       <CardText>
+//         This is an overview of the Pokemon
+//       </CardText>
+//       <Button>
+//         Button
+//       </Button>
+//     </CardBody>
+//   </Card>
+//                     </CardGroup>
                 )
 
         }
