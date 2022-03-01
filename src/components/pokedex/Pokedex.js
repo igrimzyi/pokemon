eac6e94865343cdc4248b58a117f3fcfd93c7695
igrimzyi <1isaiahofficial1@gmail.com>
2022-03-01 13:56:55 -0800

    Multiple Cards
    Testing how multiple cards would look at once within the web page

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -47,6 +47,31 @@ class Pokedex extends Component{
       </Button>
     </CardBody>
   </Card>
+<Card>
+    <CardImg
+      alt="Card image cap"
+      src="https://picsum.photos/318/180"
+      top
+      width="100%"
+    />
+    <CardBody>
+      <CardTitle tag="h5">
+        Pokemon
+      </CardTitle>
+      <CardSubtitle
+        className="mb-2 text-muted"
+        tag="h6"
+      >
+        Level
+      </CardSubtitle>
+      <CardText>
+        This is an overview of the Pokemon
+      </CardText>
+      <Button>
+        Favorite
+      </Button>
+    </CardBody>
+  </Card>
                     </CardGroup>
 
 </div>
