061c34a921da78112f125ca94fd6abf6c6aa8ad3
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-07 10:47:27 -0800

    get req to poke api
    get request to poke api that will recieve most information and be used the most in order to send the info such as sprites/stats/name

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,24 @@ app.get('/', (req, res) => {
   try{
     res.send('hello world')
   }catch(err){
-    res.send(console.error(err))
+    res.send(console.error(err));
   }
   
+})
+
+app.get('https://pokeapi.co/api/v2/pokemon', (req,res) =>{
+  try{
+    res.json();
+
+
+  }catch(err){
+    console.error(message.err);
+
+
+  }
+
+
+
 })
 
 app.listen(PORT, () =>{
