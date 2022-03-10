516372de8e9c82ff502b2805d10333d548211e9d
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-10 15:32:04 -0800

    implement cors :white_check_mark:
    cors to allow two different local host to be communicate

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,18 @@ connectDB();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
+
+
+
+app.get('/pokedex', (req, res) => {
   try{
     res.json({name : 'hello'});
   }catch(err){
