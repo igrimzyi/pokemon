91c5c52209077c6c60ab6b2e385393c54525f0ad
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-14 11:15:51 -0700

    Pokemon/:userId Component :white_check_mark:
    Component for the pokemon information to be displayed and rendered

---
+++ src/components/pokemon/Pokemon.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'; 
+
+export default class PokemonInformation extends Component {
+    render(){
+        return(
+            <div className='title'>hello</div>
+
+        )
+
+
+
+
+    }
+
+
+
+}
