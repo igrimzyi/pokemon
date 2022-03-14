fdfb2324aa5a296236e599d3bab9f1d01a503c53
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-14 11:13:06 -0700

    Dynamic Routing
    Added dynamic routed in order to to grab the user id of the pokemon and allow the user to see the render details of the pokemon.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import NavbarReact from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Pokedex from './components/pokedex/Pokedex';
 import Game from './components/Game';
-import { Component } from 'react';
 import Register from './components/register/register';
-
-
-//hello
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Pokemon from './components/pokemon/Pokemon';
 import Login from './components/login/Login';
+
+
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 // this is my main app component and will be the heart of the poke project for now 
 //contains the usage of react router and created as a component
  class App extends Component{
@@ -27,7 +27,9 @@ import Login from './components/login/Login';
             <Route path='/pokedex' element={<Pokedex />}/>
             <Route path='/game' element={<Game/>}/>
             <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}></Route>
+            <Route path='/register' element={<Register/>}/>
+          {/*dynamic routing in the navbar component  */}
+            <Route path='pokemon/:pokeId' element={<Pokemon/>}/>
            </Routes>
        
       </Router>
