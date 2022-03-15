a6050179eb63efc7018ef877dcc8f216b46c3529
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-15 12:34:14 -0700

    updating the state of a component
    the get request information is now being rendered and i am allowed to move forward with rendering my information.

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,33 +1,41 @@
 import axios from 'axios';
-import React, {Component} from 'react'; 
+import React, {Component, useState} from 'react'; 
 import { useParams } from 'react-router-dom';
-import { CardGroup, 
+import { 
+    CardGroup, 
     Card, 
     CardImg, 
     CardSubtitle,
     CardText, 
     CardBody, 
     CardTitle, 
-    Button} from "reactstrap";
+    Button
+    } from "reactstrap";
 
     export default function PokemonInformation() {
         let {pokeId} = useParams();
         console.log(pokeId)
         let baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+        const [pokeData, setPokeData] = useState('');
+        
         
 
         //get the pokemon information and render that specific pokemon on to the page!
         axios.get(`${baseUrl}${pokeId}`)
             .then((res)=>{
                 let data = res.data
-                let pokeUrl = data.sprites.front_default;
-                console.log(data)
+           
+                let imgURL = data.sprites.front_default;
+                setPokeData(imgURL);
+               console.log(pokeData)
             })
             .catch((err) =>{
                 console.log('err: ' + err)
 
             })
-            console.log()
+            
+           
             
          
         //only returning the pokemon name as of right now 
@@ -35,14 +43,11 @@ import { CardGroup,
         <div className='title'>
             <CardBody>
                 <CardImg 
-                src={function imageUrl(){
-                    return 'hello'
-                }}
+                src={pokeData}
                 top
                 className="image-margins"
                 width="25%"
                 height="50%"
-                
                 >
 
                 </CardImg>
