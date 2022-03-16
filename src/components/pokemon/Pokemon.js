f22608f09f20d7c37ae78331ed20e4ca97322fd9
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-16 11:21:42 -0700

    implemented stats for the pokemon :white_check_mark:
    Used a progress bar to give a representation of the experience and also setState for attack, defense, and health

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -9,7 +9,9 @@ import {
     CardText, 
     CardBody, 
     CardTitle, 
-    Button
+    Button,
+    Progress,
+   
     } from "reactstrap";
 
     export default function PokemonInformation() {
@@ -21,8 +23,11 @@ import {
         const [pokeData, setPokeData] = useState('');
         const [pokeExperience, setPokeExperience] = useState('');
         const [pokeType, setPokeType] = useState('');
-        const [pokeDefaultInfo, setPokeDefaultInfo] = useState('');
+        const [pokeHealth, setPokeHealth] = useState('');
+        const [pokeAttack, setPokeAttack] = useState('');
+        const [pokeDefense, setPokeDefense] = useState('');
         const [errorRender, setErrorRender] = useState('');
+        const [pokeName, setPokeName] = useState('')
         
         
 
@@ -30,25 +35,31 @@ import {
         axios.get(`${baseUrl}${pokeId}`)
             .then((res)=>{
                 let data = res.data;
+                console.log(data)
                 let imgURL = data.sprites.front_default;
+                setPokeName(data.name)
                 setPokeData(imgURL);
                 setPokeExperience(data.base_experience)
                 setPokeType(data.types[0].type.name);
+                setPokeHealth(data.stats[0].base_stat);
+                setPokeAttack(data.stats[1].base_stat);
+                setPokeDefense(data.stats[2].base_stat);
                 console.log(data)
             })
             .catch((err) =>{
                 console.log( + err)
                 setErrorRender(err);
             })
-    //returning a 404 page not found res to the user.
-            if(errorRender !== ''){
-                return(
-                    <h3 className='title'>404</h3>
-                )
 
-            }
+    // //returning a 404 page not found res to the user.
+    //         if(errorRender !== ''){
+    //             return(
+    //                 <h3 className='title'>404</h3>
+    //             )
+
+    //         }
             //returning an empty card during the loading phase of the react state
-            else if(!pokeData ){
+            if(!pokeData){
                 return(
         <div className='title'>
         <Card>
@@ -56,12 +67,7 @@ import {
                <CardText tag='h2'>
                    Loading...
                </CardText>
-                {/* <CardTitle tag='h5' className=''>
-                {pokeId}
-                </CardTitle>
-                <CardText>
-                {pokeExperience}
-                </CardText> */}
+               
             </CardBody>
             </Card>
            
@@ -82,15 +88,47 @@ import {
                 height="80%"
                 />  
                 <CardTitle tag='h5' className=''>
-                {pokeId}
+                    {pokeName}
                 </CardTitle>
                 <CardText>
-                {pokeType} 
+                    {pokeType} 
                 </CardText>
                 <CardText>
-                Default exp: {pokeExperience}
+                    Default exp: {pokeExperience}
                 </CardText>
-                
+
+                {/* setting the stats of the pokemon */}
+                <div className="text-center">
+                    {pokeHealth} of 255
+                </div>
+                 <Progress
+                max="255"
+                value={pokeHealth}
+                color='danger'
+                >
+                health
+                </Progress>
+                <div className="text-center">
+                    {pokeAttack} of 255
+                </div>
+                <Progress
+                max="255"
+                value={pokeAttack}
+                color='danger'
+                >
+                Attack
+                </Progress>
+                <div className="text-center">
+                    {pokeDefense} of 255
+                </div>
+                <Progress
+                max="255"
+                value={pokeDefense}
+                color='primary'
+                >
+                Defense
+                </Progress>
+    
              
             </CardBody>
             </Card>
