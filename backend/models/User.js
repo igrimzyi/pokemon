4a83710d4f6d09e4090c471971f0d7938e07d05e
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-20 19:35:08 -0700

    User Schema finished! :white_check_mark:
    finished user data model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 
 const UserSchema = new mongoose.Schema({
-  
+    name:{
+      type: String,
+      required: true
+    }, 
     email:{
     type:String, 
     required: true
@@ -11,8 +14,14 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:true
 
+    }, 
+    date: {
+      type: Date, 
+      default: Date.now
     }
 
 
 
 })
+
+module.exports = User = mongoose.model('user', UserSchema)
