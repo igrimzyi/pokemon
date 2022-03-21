1e4d5f36aa01a447acfbc79935c6e0609b71ddeb
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-20 18:51:58 -0700

    User validation route :white_check_mark:
    Checking if user email and username is valid

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const {check , validationResult} = require('express-validator');
+const router = express.Router();
 
 
 
 //These routes will be specifically designed to orchestrate my endpoint to create read update and delete users.
+
+router.post('/', [
+    check('name', 'name is required' )
+    .not()
+    .isEmpty(), 
+    check('email', 'Enter a valid email')
+    .isEmail(), 
+    
+
+
+])
