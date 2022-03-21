bb4331b705b13a457d482e312a56c16d402cfadb
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-21 13:55:45 -0700

    Name change and deprecated code :white_check_mark:
    fixed deprecated code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/pokeapp')
+        await mongoose.connect('mongodb://localhost:27017/pokeapplication', {useNewUrlParser: true, useUnifiedTopology: true})
         console.log('Database connected')
     }catch(err){
         console.error(err.message)
+        process.exit(1)
 
     }
 }
