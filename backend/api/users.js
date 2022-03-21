6ad23458d2cf0d8d5dda475149f6ec88da8ba5bc
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-20 18:28:15 -0700

    User validations & more :white_check_mark:
    bootstrapping my users api and creating my CRUD functionality for my users route

---
+++ backend/api/users.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const {check , validationResult} = require('express-validator');
+
+
+
+//These routes will be specifically designed to orchestrate my endpoint to create read update and delete users.
