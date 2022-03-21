f7a7dd445b686b0f1f8e2e78373bceaad1fd8ef3
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-20 18:08:39 -0700

    purge code :fire:
    Purging more code and starting backend

--- backend/api/pokedex.js
+++ backend/api/pokedex.js
@@ -7,31 +7,6 @@ const imageArray = [];
 const expArray = [];
 const typeArray = [];
 
-for(let i = 1; i< 10; i++){
-    router.get(`https://pokeapi.co/api/v2/pokemon/${i}`, (req,res)=>{
-        try{
-            const data = res.data;
-            expArray.push(data.base_experience)
-            imageArray.push(data.sprites.front_default)
-            typeArray.push(data.types[0].type.name)
-            console.log(imageArray);
 
-        }catch(error){
-            console.log('***', error)
-         }
-        }
-        
-        )
-        }
-
-server.get('/pokemon', (req,res) => {
-    try{
-        res.send(imageArray)
-    }catch(err){
-
-
-    }
-
-})
 
 module.exports = router;
