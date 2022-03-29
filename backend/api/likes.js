2b3de6cefa1309b602e0a2c9b8261e0e8474d0ad
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-03-29 07:35:23 -0700

    Update likes.js

--- backend/api/likes.js
+++ backend/api/likes.js
@@ -2,20 +2,29 @@ const Profile = require('../models/Profile');
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-import {authToken} from '../middleware/authMiddle';
+const {check , validationResult} = require('express-validator'); 
 
-router.get('/', authToken, (req,res,next)=>{
+
+
+
+router.get('/', (req,res)=>{
+ 
     try{
-        const likes = Profile.findById()
+        const profile = Profile.findById(req.body._id)
+        const likes = profile.likes
+        res.send(likes)  
     }catch(err){
-
+        res.status(500).send("Server Error" + err.message)
     }
 })
 
-router.patch('/', authToken, (req,res)  =>{
-    try{
-        
-    }catch(err){
-        res.status(500).send("Server Error" + err)
-    }
-} )
+
+// router.patch('/', authToken, (req,res)  =>{
+//     try{
+
+//     }catch(err){
+//         res.status(500).send("Server Error" + err)
+//     }
+// } )
+
+module.exports = router;
