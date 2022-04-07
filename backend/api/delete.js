d3b8513e022e14b382a7b5e7e473a07c004793e0
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-07 12:47:05 -0700

    Delete Route :white_check_mark:
    All good to go with the delete route...

---
+++ backend/api/delete.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const {check , validationResult} = require('express-validator');
+const router = express.Router();
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
+
+router.post('/', authenticateToken, async(req,res)=>{
+    try{
+        console.log(req.body); 
+        await Profile.updateOne({email:req.user.name}, {$pull:{likes:req.body.pokemon}})
+        return res.status(204).send('deleted like')
+    }catch(err){
+      return res.status(500).send(err)
+    }
+})
+
+
+//auth middleware
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.status(401).send('forbidden')
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) =>{
+        if(err) return res.status(403).send('forbidden token');
+        req.user = user
+        next();
+    })
+}
+
+module.exports = router; 
