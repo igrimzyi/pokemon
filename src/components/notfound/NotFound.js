45e03a3b646e822b69509922e75bfe42e256b8f0
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-09 15:19:51 -0700

    404 Not Found :white_check_mark:
    404 not found is partially working still need to cover some routes

---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './notfound.css'; 
+
+
+export default class NotFound extends Component{
+
+
+    render(){
+        return(
+            <div className='not-found-styles'>not found lol </div>
+
+
+        )
+    }
+
+
+}
