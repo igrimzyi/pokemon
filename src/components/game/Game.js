67625a48b03abe80589277a564d8f799ad43dd9a
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-09 16:38:21 -0700

    Added a bit of protection :white_check_mark:
    If the user was not logged in then they would be redirected to the login route due to a newly implemented leveling system

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,16 +1,47 @@
 import { Button } from "reactstrap";
 import React,{ Component } from "react";
+import axios from 'axios';
+import Login from '../login/Login'; 
+
+import {Navigate,Route, Routes} from 'react-router-dom'
+
 import './game.css'
+
 
 class Game extends Component {
         constructor(props){
                 super(props);
 
                 this.state ={
-                        isStarted:false
+                        isStarted:false,
+                        profile:{}, 
+                        isLoggedIn:{}
+                        
                 }
 
                 this.handleClickStart = this.handleClickStart.bind(this)
+        }
+
+        componentDidMount(){
+                const config = {
+                        headers:{
+                            Authorization: "Bearer " + localStorage.getItem('userToken')
+                        }
+                    }
+
+                axios.get('http://localhost:4000/api/profile', config)
+                .then((res)=>{
+                        this.setState({
+                                profile: res
+                        })
+                })
+                .catch((err)=>{
+                       this.setState({
+                               isLoggedIn:err.response.status
+                       })
+                 
+                      
+                })
         }
 
         handleClickStart(){
@@ -20,19 +51,27 @@ class Game extends Component {
         }
 
         render(){
-                if(this.state.isStarted === false){
+                if(this.state.isLoggedIn){
+                        
+                        return(
+                                 <Navigate to='/login' replace={true}></Navigate>
+                        )
+                }
+
+               if(this.state.isStarted === false){
                         return (
                                 <div className="button-view">
                                 <Button className="center-content" onClick={this.handleClickStart}>Start Game</Button>
                                 </div>
                         )
-                }
+                }else  {
             return(
                     <div className="button-view" >
                 <h1 className="center-content" >hello world</h1>
                 </div>
                         
                 )
+            }
         }
 
 }
