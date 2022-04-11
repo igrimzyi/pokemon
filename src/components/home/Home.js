75158d52d6b78c60e63aa17ce59f581cc1b668e5
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-10 17:22:03 -0700

    Changing UI and adding New Display :white_check_mark:
    Making the frontend display for mobile and web browser better

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,35 @@
 import React, {Component} from "react";
 import './home.css'
 import {Button} from 'reactstrap';
+import axios from 'axios';
 
 import NavbarReact from "../navbar/Navbar";
 
  class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            pokeData:{}
+        }
+    }
+
+    componentDidMount(){
+        const baseUrl =  "https://pokeapi.co/api/v2/pokemon/25"
+
+        axios
+        .get(baseUrl)
+        .then((res)=>{
+            this.setState({
+                pokeData: res.data.sprites.front_default
+            })
+
+            console.log(this.state.pokeData)
+        })
+        .catch((err)=>{
+            return err
+        })
+    }
         render(){
             return(
 
@@ -18,15 +43,16 @@ import NavbarReact from "../navbar/Navbar";
                         <h3 className="">Welcome to Poke Complex</h3>
                         <span>Check out the pokedex and the game that comes with!</span>
                         <div>
-                    <Button
-                    color="dark"
-                    size=""
-                    href="/pokedex"
-                    className="button"
-                    >
-                    View Pokedex
-                    </Button>
-                    </div>
+                                <Button
+                                color="dark"
+                                size=""
+                                href="/pokedex"
+                                className="button"
+                                >
+                                View Pokedex
+                                </Button>
+                        </div>
+                        <img className="pokemon-display" src={this.state.pokeData}></img>
                     </div>
                     
                     
