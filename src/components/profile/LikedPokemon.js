36584d9d514b55e4ec836ace64ea634f01a07b4a
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-15 13:31:11 -0700

    transfered code :white_check_mark:
    Moved code over to Profiles folder

---
+++ src/components/profile/LikedPokemon.js
@@ -0,0 +1,144 @@
+import React, {Component} from 'react';
+import { Navigate } from 'react-router-dom';
+import {
+    CardGroup,
+    Card, 
+    CardImg, 
+    CardSubtitle,
+    CardText, 
+    CardBody, 
+    CardTitle, 
+    Button } from 'reactstrap';
+const axios = require('axios');
+
+
+
+
+
+
+class PokemonDetails extends Component {
+    constructor(props) {
+   
+    super(props);
+
+    this.state = {
+      loading: true,
+      details: {}
+    }
+  }
+
+  componentDidMount() {
+      
+    axios.get(this.props.pokemon)
+      .then((response) => {
+          console.log(this.props.pokemon)
+       
+        this.setState({ 
+          loading: false,
+          details: response.data
+        })
+     
+        
+      })
+    
+  }
+
+
+  render () {
+    if (this.state.loading) {
+      return <p>Loading...</p>
+    }
+    
+    return   <Card className='card-align liked-div'>  
+              <CardImg 
+                alt="Card image cap"
+                src={this.state.details.sprites.front_default}
+                top
+                className="image-margins"
+                width="25%"
+                height="50%"
+              />
+              <CardBody className="body-styles">
+                <CardTitle tag="h5">
+                  { this.state.details.name }
+                </CardTitle>
+                <CardSubtitle
+                  className="mb-2 text-muted"
+                  tag="h6"
+                >
+                  
+                  {/* {this.state.stats} */}
+                </CardSubtitle>
+                <CardText>
+                type: {this.state.details.types[0].type.name}  
+                </CardText>
+              </CardBody>
+            </Card>;
+  }
+}
+
+
+
+
+export default class LikedPokemon extends Component{
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            name:'',
+            likes:[],
+            errResponse:null,
+            likedPokemon:null
+
+        }
+    }
+   
+    componentDidMount(){
+        const config= {
+            headers:{
+                Authorization: "Bearer " + localStorage.getItem('userToken')
+            }
+        }
+
+        axios.get('http://localhost:4000/api/likes', config)
+            .then((res)=>{
+                this.setState({
+                    likes: res.data.likes
+                })
+                console.log(res.data.likes)
+            })
+            .catch((err)=>{
+                this.setState({
+                    errResponse:err.response.status
+                })
+            })
+        }
+
+
+    render(){
+
+        if(this.state.errResponse === 401 || this.state.errResponse === 403){
+            return(
+                <Navigate to='/login' replace={true}/>
+            )
+        } else if(this.state.likes.length === 0){
+            return(
+                <div className='liked-div'>
+                    oops you have not liked any pokemon yet!
+                </div>
+            )
+        }else
+        
+        return(
+        <div className='container'>
+            <div>
+                {this.state.likes.map((pokemon, i) =>{
+                    return <PokemonDetails key={i} pokemon={pokemon}></PokemonDetails>  
+                })}
+            </div>
+
+        </div>
+        )
+    }
+    }
+
