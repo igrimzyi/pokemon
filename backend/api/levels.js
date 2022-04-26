33af619063e5d6051a4bb615748edc18110dd35c
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-04-26 11:17:21 -0700

    JSON information :white_check_mark:
    sending JSON information that will send you being leveled up with the class if and remainding exp

--- backend/api/levels.js
+++ backend/api/levels.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const Profile = require('../models/Profile'); 
 const jwt = require('jsonwebtoken')
 
-let trainer = 'Pokémon Trainer'
 let leader =  'Gym Leader'
 let champion = 'Pokémon Champion'
 
@@ -13,22 +12,36 @@ let champion = 'Pokémon Champion'
 router.patch('/', authenticateToken, async(req,res)=>{
     try{
         // console.log(req.body);
-        // console.log(req.user);
+        console.log(req.user.email);
     
         let profile = await Profile.findOne({email:req.user.email})
-        if(profile.class === trainer){
+        if(profile.class === 'Pokemon Trainer'){
             let max = 100; 
             let randomNum =  Math.floor(Math.random() * 20 ) + 5; 
             let newLevel = profile.experience + randomNum ; 
             if(max <= newLevel){
                 let newExperience = newLevel - max; 
-                Profile.findOneAndUpdate({email:req.user.email},{experience:newExperience});
-                Profile.findOneAndUpdate({email:req.user.email},{experience:newExperience});
+                await Profile.findOneAndUpdate({email:req.user.email},{experience:newExperience});
+                await Profile.findOneAndUpdate({email:req.user.email},{class:champion});
+                return res.json({msg:`You have leveled up to ${champion}!`, exp:newExperience}).status(200);
+            }else{ 
+                await Profile.findOneAndUpdate({email:req.user.email},{experience:newLevel});
+                return res.json({msg:'You have gained experience', exp:newLevel}).status(200)
+            }
+        
+        }else if (profile.class === champion){
+     
+      
+            let newLevel = 1 ; 
+            if(max <= newLevel){
+                await Profile.findOneAndUpdate({email:req.user.email},{experience:newLevel});
+                await Profile.findOneAndUpdate({email:req.user.email},{class:leader});
+                return res.json({msg:`You have leveled up to ${leader}!`, exp:newLevel}).status(200);
             }
 
         }
         console.log(profile.experience)    
-        res.send('you leveled up!').status(200)
+        
     }catch(err){
         console.log(err)
     }
